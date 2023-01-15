fb0e45aa51ce9b80c17c7196e1638e69a326cd48
Chris McGalliard <65915981+mcgalliard@users.noreply.github.com>
2023-01-14 21:29:49 -0500

    Added more to velocity parse

--- source/navigation/gps/gps.h
+++ source/navigation/gps/gps.h
@@ -19,11 +19,31 @@ union i_Long
     signed long iLong;
 };
 
+union i_Short
+{
+    uint8_t bytes[2];
+    signed short iShort;
+};
+
 typedef struct
 {
     uint8_t raw_message[100];
     uint8_t g_speed_bytes[4];
     signed long g_speed;
+    uint8_t longitude_bytes[4];
+    signed long longitude;
+    uint8_t latitude_bytes[4];
+    signed long latitude;
+    uint8_t height_bytes[4];
+    signed long height;
+    uint8_t n_vel_bytes[4];
+    signed long n_vel;
+    uint8_t e_vel_bytes[4];
+    signed long e_vel;
+    uint8_t d_vel_bytes[4];
+    signed long d_vel;
+    uint8_t mag_dec_bytes[2];
+    signed short mag_dec;
 } GPS_Handle_t; // GPS handle
 
 #endif //_GPS_H
