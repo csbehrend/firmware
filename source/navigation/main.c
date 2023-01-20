504552c54724266d446c077d9e99191b1dc53fcd
Chris McGalliard <65915981+mcgalliard@users.noreply.github.com>
2023-01-20 15:30:12 -0500

    Begin SPI2 config

--- source/navigation/main.c
+++ source/navigation/main.c
@@ -13,6 +13,8 @@
 #include "main.h"
 #include "gps.h"
 
+uint8_t collect_test[100] = {0};
+
 GPIOInitConfig_t gpio_config[] = {
     // Status Indicators
     GPIO_INIT_OUTPUT(ERR_LED_GPIO_Port, ERR_LED_Pin, GPIO_OUTPUT_LOW_SPEED),
@@ -20,55 +22,58 @@ GPIOInitConfig_t gpio_config[] = {
     GPIO_INIT_OUTPUT(HEARTBEAT_GPIO_Port, HEARTBEAT_Pin, GPIO_OUTPUT_LOW_SPEED),
 
     // SPI
-    GPIO_INIT_AF(SPI_SCLK_GPIO_Port, SPI_SCLK_Pin,  5, GPIO_OUTPUT_HIGH_SPEED, GPIO_OUTPUT_PUSH_PULL, GPIO_INPUT_PULL_DOWN),
-    GPIO_INIT_AF(SPI_MOSI_GPIO_Port, SPI_MOSI_Pin,  5, GPIO_OUTPUT_HIGH_SPEED, GPIO_OUTPUT_PUSH_PULL, GPIO_INPUT_PULL_DOWN),
-    GPIO_INIT_AF(SPI_MISO_GPIO_Port, SPI_MISO_Pin,  5, GPIO_OUTPUT_HIGH_SPEED, GPIO_OUTPUT_OPEN_DRAIN, GPIO_INPUT_OPEN_DRAIN),
+    GPIO_INIT_AF(SPI_SCLK_GPIO_Port, SPI_SCLK_Pin, 5, GPIO_OUTPUT_HIGH_SPEED, GPIO_OUTPUT_PUSH_PULL, GPIO_INPUT_PULL_DOWN),
+    GPIO_INIT_AF(SPI_MOSI_GPIO_Port, SPI_MOSI_Pin, 5, GPIO_OUTPUT_HIGH_SPEED, GPIO_OUTPUT_PUSH_PULL, GPIO_INPUT_PULL_DOWN),
+    GPIO_INIT_AF(SPI_MISO_GPIO_Port, SPI_MISO_Pin, 5, GPIO_OUTPUT_HIGH_SPEED, GPIO_OUTPUT_OPEN_DRAIN, GPIO_INPUT_OPEN_DRAIN),
     GPIO_INIT_OUTPUT(SPI_CS_ACEL_GPIO_Port, SPI_CS_ACEL_Pin, GPIO_OUTPUT_HIGH_SPEED),
     GPIO_INIT_OUTPUT(SPI_CS_GYRO_GPIO_Port, SPI_CS_GYRO_Pin, GPIO_OUTPUT_HIGH_SPEED),
     GPIO_INIT_OUTPUT(SPI_CS_MAG_GPIO_Port, SPI_CS_MAG_Pin, GPIO_OUTPUT_HIGH_SPEED),
+
+    // GPS SPI2
+    GPIO_INIT_AF(SPI2_SCLK_GPIO_Port, SPI2_SCLK_Pin, 5, GPIO_OUTPUT_HIGH_SPEED, GPIO_OUTPUT_PUSH_PULL, GPIO_INPUT_PULL_DOWN),
+    GPIO_INIT_AF(SPI2_MOSI_GPIO_Port, SPI2_MOSI_Pin, 5, GPIO_OUTPUT_HIGH_SPEED, GPIO_OUTPUT_PUSH_PULL, GPIO_INPUT_PULL_DOWN),
+    GPIO_INIT_AF(SPI2_MISO_GPIO_Port, SPI2_MISO_Pin, 5, GPIO_OUTPUT_HIGH_SPEED, GPIO_OUTPUT_OPEN_DRAIN, GPIO_INPUT_OPEN_DRAIN),
+    GPIO_INIT_OUTPUT(SPI2_CS_GPIO_Port, SPI2_CS_Pin, GPIO_OUTPUT_HIGH_SPEED),
 
     // GPS USART
     GPIO_INIT_USART3RX_PC5,
     GPIO_INIT_USART3TX_PC4,
 
-    //EEPROM 
+    // EEPROM
     GPIO_INIT_OUTPUT(NAV_EEPROM_CS_GPIO_PORT, NAV_EEPROM_CS_PIN, GPIO_OUTPUT_HIGH_SPEED),
     GPIO_INIT_OUTPUT(NAV_WP_GPIO_PORT, NAV_WP_PIN, GPIO_OUTPUT_HIGH_SPEED),
 };
-
-
 
 /* USART Configuration */
 // M9N GPS
 dma_init_t usart_gps_tx_dma_config = USART3_TXDMA_CONT_CONFIG(NULL, 1);
 dma_init_t usart_gps_rx_dma_config = USART3_RXDMA_CONT_CONFIG(NULL, 2);
 usart_init_t huart_gps = {
-    .baud_rate   = 115200,
+    .baud_rate = 115200,
     .word_length = WORD_8,
     .hw_flow_ctl = HW_DISABLE,
-    .mode        = MODE_TX_RX,
-    .stop_bits   = SB_ONE,
-    .parity      = PT_NONE,
-    .obsample    = OB_DISABLE,
-    .ovsample    = OV_16,
-    .adv_feature.rx_inv    = false,
-    .adv_feature.tx_inv    = false,
+    .mode = MODE_TX_RX,
+    .stop_bits = SB_ONE,
+    .parity = PT_NONE,
+    .obsample = OB_DISABLE,
+    .ovsample = OV_16,
+    .adv_feature.rx_inv = false,
+    .adv_feature.tx_inv = false,
     .adv_feature.auto_baud = false,
-    .adv_feature.data_inv  = false,
+    .adv_feature.data_inv = false,
     .adv_feature.msb_first = false,
-    .adv_feature.overrun   = false,
+    .adv_feature.overrun = false,
     .adv_feature.dma_on_rx_err = false,
     .tx_dma_cfg = &usart_gps_tx_dma_config,
-    .rx_dma_cfg = &usart_gps_rx_dma_config
-};
+    .rx_dma_cfg = &usart_gps_rx_dma_config};
 
 #define TargetCoreClockrateHz 16000000
 ClockRateConfig_t clock_config = {
-    .system_source              =SYSTEM_CLOCK_SRC_HSI,
-    .system_clock_target_hz     =TargetCoreClockrateHz,
-    .ahb_clock_target_hz        =(TargetCoreClockrateHz / 1),
-    .apb1_clock_target_hz       =(TargetCoreClockrateHz / (1)),
-    .apb2_clock_target_hz       =(TargetCoreClockrateHz / (1)),
+    .system_source = SYSTEM_CLOCK_SRC_HSI,
+    .system_clock_target_hz = TargetCoreClockrateHz,
+    .ahb_clock_target_hz = (TargetCoreClockrateHz / 1),
+    .apb1_clock_target_hz = (TargetCoreClockrateHz / (1)),
+    .apb2_clock_target_hz = (TargetCoreClockrateHz / (1)),
 };
 
 /* Locals for Clock Rates */
@@ -82,13 +87,24 @@ dma_init_t spi_tx_dma_config = SPI1_TXDMA_CONT_CONFIG(NULL, 1);
 
 SPI_InitConfig_t spi_config = {
     .data_rate = TargetCoreClockrateHz / 64,
-    .data_len  = 8,
+    .data_len = 8,
     .nss_sw = true,
     .nss_gpio_port = SPI_CS_GYRO_GPIO_Port,
     .nss_gpio_pin = SPI_CS_GYRO_Pin,
     .rx_dma_cfg = &spi_rx_dma_config,
-    .tx_dma_cfg = &spi_tx_dma_config
-};
+    .tx_dma_cfg = &spi_tx_dma_config};
+
+dma_init_t spi2_rx_dma_config = SPI2_RXDMA_CONT_CONFIG(NULL, 2);
+dma_init_t spi2_tx_dma_config = SPI2_TXDMA_CONT_CONFIG(NULL, 1);
+
+SPI_InitConfig_t spi2_config = {
+    .data_rate = TargetCoreClockrateHz / 64,
+    .data_len = 100,
+    .nss_sw = true,
+    .nss_gpio_port = SPI2_CS_GPIO_Port,
+    .nss_gpio_pin = SPI2_CS_Pin,
+    .rx_dma_cfg = &spi2_rx_dma_config,
+    .tx_dma_cfg = &spi2_tx_dma_config};
 
 BMI088_Handle_t bmi_config = {
     .accel_csb_gpio_port = SPI_CS_ACEL_GPIO_Port,
@@ -100,8 +116,7 @@ BMI088_Handle_t bmi_config = {
     .gyro_csb_pin = SPI_CS_GYRO_Pin,
     .gyro_datarate = GYRO_DR_100Hz_32Hz,
     .gyro_range = GYRO_RANGE_250,
-    .spi = &spi_config
-};
+    .spi = &spi_config};
 
 IMU_Handle_t imu_h = {
     .bmi = &bmi_config,
@@ -116,41 +131,59 @@ void sendIMUData(void);
 extern void HardFault_Handler(void);
 void collectGPSData(void);
 
-int main (void)
+int main(void)
 {
     /* Data Struct Initialization */
     // qConstruct(&q_tx_can, sizeof(CanMsgTypeDef_t));
     // qConstruct(&q_rx_can, sizeof(CanMsgTypeDef_t));
 
     /* HAL Initialization */
-    if(0 != PHAL_configureClockRates(&clock_config))
+    if (0 != PHAL_configureClockRates(&clock_config))
     {
         HardFault_Handler();
     }
-    if(!PHAL_initGPIO(gpio_config, sizeof(gpio_config)/sizeof(GPIOInitConfig_t)))
-    {
-        HardFault_Handler();
-    }
-
-    if(!PHAL_initUSART(USART3, &huart_gps, APB1ClockRateHz))
+    if (!PHAL_initGPIO(gpio_config, sizeof(gpio_config) / sizeof(GPIOInitConfig_t)))
     {
         HardFault_Handler();
     }
 
-    spi_config.data_rate = APB2ClockRateHz / 16;
-    if (!PHAL_SPI_init(&spi_config))
+    // huart_gps.rx_dma_cfg->circular = true;
+    // if (!PHAL_initUSART(USART3, &huart_gps, APB1ClockRateHz))
+    // {
+    //     HardFault_Handler();
+    // }
+    // PHAL_usartRxDma(USART3, &huart_gps, (uint16_t *)collect_test, 100);
+
+    if (!PHAL_SPI_init(&spi2_config))
+    {
         HardFault_Handler();
+    }
+    // spi_config.data_rate = APB2ClockRateHz / 16;
+    // spi2_config.data_rate = APB2ClockRateHz / 16;
+    // static uint8_t spi2_rx_buff[100] = {0};
+    // static uint8_t spi2_tx_buff[100] = {0};
+    // PHAL_SPI_transfer(&spi2_config, spi2_tx_buff, 100, &spi2_rx_buff);
+
+    // if (!PHAL_SPI_init(&spi_config))
+    //     HardFault_Handler();
     PHAL_writeGPIO(SPI_CS_ACEL_GPIO_Port, SPI_CS_ACEL_Pin, 0);
     PHAL_writeGPIO(SPI_CS_GYRO_GPIO_Port, SPI_CS_GYRO_Pin, 1);
     PHAL_writeGPIO(SPI_CS_MAG_GPIO_Port, SPI_CS_MAG_Pin, 1);
+    // while (1)
+    // {
+    //     PHAL_usartRxBl(USART3, (uint16_t *)collect_test, 100);
+    //     PHAL_toggleGPIO(ERR_LED_GPIO_Port, ERR_LED_Pin);
+    // }
 
     /* Task Creation */
     schedInit(APB1ClockRateHz);
     configureAnim(preflightAnimation, preflightChecks, 74, 750);
 
     taskCreate(heartBeatLED, 500);
-    taskCreate(sendIMUData, 10);
-    taskCreate(collectGPSData, 100);
+
+    // taskCreate(sendIMUData, 10);
+    // taskCreate(collectGPSData, 1000);
+
     // taskCreateBackground(canTxUpdate);
     // taskCreateBackground(canRxUpdate);
 
@@ -160,47 +193,49 @@ int main (void)
     return 0;
 }
 
-void preflightChecks(void) {
+void preflightChecks(void)
+{
     static uint16_t state;
 
     switch (state++)
     {
-        case 0:
-            // if(!PHAL_initCAN(CAN1, false))
-            // {
-            //     HardFault_Handler();
-            // }
-            // NVIC_EnableIRQ(CAN1_RX0_IRQn);
-           break;
-        case 1:
-            if (!BMI088_init(&bmi_config))
-                HardFault_Handler();
-            break;
-        case 100:
-            // Put accel into SPI mode
-            PHAL_writeGPIO(SPI_CS_ACEL_GPIO_Port, SPI_CS_ACEL_Pin, 1);
-            break;
-        case 250:
-            BMI088_powerOnAccel(&bmi_config);
-            break;
+    case 0:
+        // if(!PHAL_initCAN(CAN1, false))
+        // {
+        //     HardFault_Handler();
+        // }
+        // NVIC_EnableIRQ(CAN1_RX0_IRQn);
+        break;
+    // case 1:
+    //     if (!BMI088_init(&bmi_config))
+    //         HardFault_Handler();
+    //     break;
+    case 100:
+        // Put accel into SPI mode
+        PHAL_writeGPIO(SPI_CS_ACEL_GPIO_Port, SPI_CS_ACEL_Pin, 1);
+        break;
+    // case 250:
+    //     BMI088_powerOnAccel(&bmi_config);
+    //     break;
 
-        case 500:
-            if (!BMI088_initAccel(&bmi_config))
-                HardFault_Handler();
-            break;
-        default:
-            if (state > 750)
-            {
-                if (!imu_init(&imu_h))
-                    HardFault_Handler();
-                registerPreflightComplete(1);
-                state = 255; // prevent wrap around
-            }
-            break;
+    // case 500:
+    //     if (!BMI088_initAccel(&bmi_config))
+    //         HardFault_Handler();
+    //     break;
+    default:
+        if (state > 750)
+        {
+            // if (!imu_init(&imu_h))
+            // HardFault_Handler();
+            registerPreflightComplete(1);
+            state = 255; // prevent wrap around
+        }
+        break;
     }
 }
 
-void preflightAnimation(void) {
+void preflightAnimation(void)
+{
     static uint32_t time;
 
     PHAL_writeGPIO(HEARTBEAT_GPIO_Port, HEARTBEAT_Pin, 0);
@@ -209,24 +244,24 @@ void preflightAnimation(void) {
 
     switch (time++ % 6)
     {
-        case 0:
-        case 5:
-            PHAL_writeGPIO(HEARTBEAT_GPIO_Port, HEARTBEAT_Pin, 1);
-            break;
-        case 1:
-        case 4:
-            PHAL_writeGPIO(CONN_LED_GPIO_Port, CONN_LED_Pin, 1);
-            break;
-        case 2:
-        case 3:
-            PHAL_writeGPIO(ERR_LED_GPIO_Port, ERR_LED_Pin, 1);
-            break;
+    case 0:
+    case 5:
+        PHAL_writeGPIO(HEARTBEAT_GPIO_Port, HEARTBEAT_Pin, 1);
+        break;
+    case 1:
+    case 4:
+        PHAL_writeGPIO(CONN_LED_GPIO_Port, CONN_LED_Pin, 1);
+        break;
+    case 2:
+    case 3:
+        PHAL_writeGPIO(ERR_LED_GPIO_Port, ERR_LED_Pin, 1);
+        break;
     }
 }
 void heartBeatLED(void)
 {
     PHAL_toggleGPIO(HEARTBEAT_GPIO_Port, HEARTBEAT_Pin);
-    
+
     // if ((sched.os_ticks - last_can_rx_time_ms) >= CONN_LED_MS_THRESH)
     //      PHAL_writeGPIO(CONN_LED_GPIO_Port, CONN_LED_Pin, 0);
     // else PHAL_writeGPIO(CONN_LED_GPIO_Port, CONN_LED_Pin, 1);
@@ -237,12 +272,13 @@ void sendIMUData(void)
     imu_periodic(&imu_h);
 }
 
-//Test Nav Message
-GPS_Handle_t testGPSHandle = {0xB5, 0x62, 0x01, 0x07, 0x5C, 0x00, 0x80, 0x10, 0xC1, 0x08, 0xE7, 0x07, 0x01, 0x02, 0x10, 0x2F, 0x20, 0xF3, 0xFF, 0xFF, 0xFF, 0xFF, 0xB0, 0xB1, 0xD8, 0x17, 0x03, 0x01, 0xEA, 0x05, 0x32, 0x3B, 0xEC, 0xCB, 0x92, 0x1D, 0xA7, 0x16, 0xBF, 0xB6, 0x01, 0x00, 0xD2, 0x34, 0x02, 0x00, 0x85, 0x0D, 0x00, 0x00, 0x8A, 0x29, 0x00, 0x00, 0x04, 0x00, 0x00, 0x00, 0x0E, 0x00, 0x00, 0x00, 0x0D, 0x00, 0x00, 0x00, 0x0E, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x0E, 0x01, 0x00, 0x00, 0x72, 0x1A, 0xFC, 0x00, 0x6B, 0x01, 0x00, 0x00, 0xEE, 0x13, 0x4F, 0x2F, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x41, 0x97};
+// Test Nav Message
+GPS_Handle_t testGPSHandle = {0x00, 0x62, 0x01, 0x07, 0x5C, 0x00, 0x80, 0x10, 0xC1, 0x08, 0xE7, 0x07, 0x01, 0x02, 0x10, 0x2F, 0x20, 0xF3, 0xFF, 0xFF, 0xFF, 0xFF, 0xB0, 0xB1, 0xD8, 0x17, 0x03, 0x01, 0xEA, 0x05, 0x32, 0x3B, 0xEC, 0xCB, 0x92, 0x1D, 0xA7, 0x16, 0xBF, 0xB6, 0x01, 0x00, 0xD2, 0x34, 0x02, 0x00, 0x85, 0x0D, 0x00, 0x00, 0x8A, 0x29, 0x00, 0x00, 0x04, 0x00, 0x00, 0x00, 0x0E, 0x00, 0x00, 0x00, 0x0D, 0x00, 0x00, 0x00, 0x0E, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x0E, 0x01, 0x00, 0x00, 0x72, 0x1A, 0xFC, 0x00, 0x6B, 0x01, 0x00, 0x00, 0xEE, 0x13, 0x4F, 0x2F, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x41, 0x97};
 
-//Test function for usartRxDma
-void collectGPSData(void) {
-    PHAL_usartRxDma(USART3, &huart_gps, (uint16_t *) testGPSHandle.raw_message, 100);
+// Test function for usartRxDma
+void collectGPSData(void)
+{
+    // PHAL_usartRxDma(USART3, &huart_gps, (uint16_t *)collect_test, 100);
 }
 
 // void canTxUpdate(void)
@@ -304,7 +340,7 @@ void collectGPSData(void) {
 void HardFault_Handler()
 {
     PHAL_writeGPIO(ERR_LED_GPIO_Port, ERR_LED_Pin, 1);
-    while(1)
+    while (1)
     {
         __asm__("nop");
     }
--- source/precharge/main.c
+++ source/precharge/main.c
@@ -18,7 +18,6 @@
 #include "orion.h"
 #include "bsxlite_interface.h"
 
-
 /* PER HAL Initilization Structures */
 GPIOInitConfig_t gpio_config[] = {
     // CAN
@@ -28,9 +27,9 @@ GPIOInitConfig_t gpio_config[] = {
     GPIO_INIT_CAN2TX_PB13,
 
     // SPI
-    GPIO_INIT_AF(SPI_SCLK_GPIO_Port, SPI_SCLK_Pin,  5, GPIO_OUTPUT_HIGH_SPEED, GPIO_OUTPUT_PUSH_PULL, GPIO_INPUT_PULL_DOWN),
-    GPIO_INIT_AF(SPI_MOSI_GPIO_Port, SPI_MOSI_Pin,  5, GPIO_OUTPUT_HIGH_SPEED, GPIO_OUTPUT_PUSH_PULL, GPIO_INPUT_PULL_DOWN),
-    GPIO_INIT_AF(SPI_MISO_GPIO_Port, SPI_MISO_Pin,  5, GPIO_OUTPUT_HIGH_SPEED, GPIO_OUTPUT_OPEN_DRAIN, GPIO_INPUT_OPEN_DRAIN),
+    GPIO_INIT_AF(SPI_SCLK_GPIO_Port, SPI_SCLK_Pin, 5, GPIO_OUTPUT_HIGH_SPEED, GPIO_OUTPUT_PUSH_PULL, GPIO_INPUT_PULL_DOWN),
+    GPIO_INIT_AF(SPI_MOSI_GPIO_Port, SPI_MOSI_Pin, 5, GPIO_OUTPUT_HIGH_SPEED, GPIO_OUTPUT_PUSH_PULL, GPIO_INPUT_PULL_DOWN),
+    GPIO_INIT_AF(SPI_MISO_GPIO_Port, SPI_MISO_Pin, 5, GPIO_OUTPUT_HIGH_SPEED, GPIO_OUTPUT_OPEN_DRAIN, GPIO_INPUT_OPEN_DRAIN),
     GPIO_INIT_OUTPUT(SPI_CS_ACEL_GPIO_Port, SPI_CS_ACEL_Pin, GPIO_OUTPUT_HIGH_SPEED),
     GPIO_INIT_OUTPUT(SPI_CS_GYRO_GPIO_Port, SPI_CS_GYRO_Pin, GPIO_OUTPUT_HIGH_SPEED),
 
@@ -48,11 +47,11 @@ GPIOInitConfig_t gpio_config[] = {
 
 #define TargetCoreClockrateHz 16000000
 ClockRateConfig_t clock_config = {
-    .system_source              =SYSTEM_CLOCK_SRC_HSI,
-    .system_clock_target_hz     =TargetCoreClockrateHz,
-    .ahb_clock_target_hz        =(TargetCoreClockrateHz / 1),
-    .apb1_clock_target_hz       =(TargetCoreClockrateHz / (1)),
-    .apb2_clock_target_hz       =(TargetCoreClockrateHz / (1)),
+    .system_source = SYSTEM_CLOCK_SRC_HSI,
+    .system_clock_target_hz = TargetCoreClockrateHz,
+    .ahb_clock_target_hz = (TargetCoreClockrateHz / 1),
+    .apb1_clock_target_hz = (TargetCoreClockrateHz / (1)),
+    .apb2_clock_target_hz = (TargetCoreClockrateHz / (1)),
 };
 
 /* Locals for Clock Rates */
@@ -80,13 +79,12 @@ dma_init_t spi_tx_dma_config = SPI1_TXDMA_CONT_CONFIG(NULL, 1);
 
 SPI_InitConfig_t spi_config = {
     .data_rate = TargetCoreClockrateHz / 64,
-    .data_len  = 8,
+    .data_len = 8,
     .nss_sw = true,
     .nss_gpio_port = SPI_CS_GYRO_GPIO_Port,
     .nss_gpio_pin = SPI_CS_GYRO_Pin,
     .rx_dma_cfg = &spi_rx_dma_config,
-    .tx_dma_cfg = &spi_tx_dma_config
-};
+    .tx_dma_cfg = &spi_tx_dma_config};
 
 BMI088_Handle_t bmi_config = {
     .accel_csb_gpio_port = SPI_CS_ACEL_GPIO_Port,
@@ -98,15 +96,13 @@ BMI088_Handle_t bmi_config = {
     .gyro_csb_pin = SPI_CS_GYRO_Pin,
     .gyro_datarate = GYRO_DR_100Hz_32Hz,
     .gyro_range = GYRO_RANGE_250,
-    .spi = &spi_config
-};
+    .spi = &spi_config};
 
 IMU_Handle_t imu_h = {
     .bmi = &bmi_config,
 };
 
-
-int main (void)
+int main(void)
 {
     /* Data Struct init */
     qConstruct(&q_tx_can, sizeof(CanMsgTypeDef_t));
@@ -116,7 +112,7 @@ int main (void)
     if (0 != PHAL_configureClockRates(&clock_config))
         PHAL_FaultHandler();
 
-    if (1 != PHAL_initGPIO(gpio_config, sizeof(gpio_config)/sizeof(GPIOInitConfig_t)))
+    if (1 != PHAL_initGPIO(gpio_config, sizeof(gpio_config) / sizeof(GPIOInitConfig_t)))
         PHAL_FaultHandler();
 
     // set high during init
@@ -162,7 +158,6 @@ int main (void)
     return 0;
 }
 
-
 // *** Startup configuration ***
 void preflightChecks(void)
 {
@@ -170,40 +165,39 @@ void preflightChecks(void)
 
     switch (state++)
     {
-        case 0 :
-            if (daqInit(&q_tx_can, 0))
+    case 0:
+        if (daqInit(&q_tx_can, 0))
+            PHAL_FaultHandler();
+        break;
+
+    case 1:
+        if (!BMI088_init(&bmi_config))
+            PHAL_FaultHandler();
+        break;
+
+    case 100:
+        // Put accel into SPI mode
+        PHAL_writeGPIO(SPI_CS_ACEL_GPIO_Port, SPI_CS_ACEL_Pin, 1);
+        break;
+
+    case 250:
+        BMI088_powerOnAccel(&bmi_config);
+        break;
+
+    case 500:
+        if (!BMI088_initAccel(&bmi_config))
+            PHAL_FaultHandler();
+    default:
+        if (state > 750)
+        {
+            if (!imu_init(&imu_h))
                 PHAL_FaultHandler();
-            break;
-
-        case 1:
-            if (!BMI088_init(&bmi_config))
-                PHAL_FaultHandler();
-            break;
-
-        case 100:
-            // Put accel into SPI mode
-            PHAL_writeGPIO(SPI_CS_ACEL_GPIO_Port, SPI_CS_ACEL_Pin, 1);
-            break;
-
-        case 250:
-            BMI088_powerOnAccel(&bmi_config);
-            break;
-
-        case 500:
-            if (!BMI088_initAccel(&bmi_config))
-                PHAL_FaultHandler();
-        default:
-            if (state > 750)
-            {
-                if (!imu_init(&imu_h))
-                    PHAL_FaultHandler();
-                registerPreflightComplete(1);
-                state = 751;
-            }
-            break;
+            registerPreflightComplete(1);
+            state = 751;
+        }
+        break;
     }
 }
-
 
 void preflightAnimation(void)
 {
@@ -215,17 +209,17 @@ void preflightAnimation(void)
 
     switch (time++ % 3)
     {
-        case 0:
-            PHAL_writeGPIO(HEARTBEAT_LED_GPIO_Port, HEARTBEAT_LED_Pin, 1);
-            break;
+    case 0:
+        PHAL_writeGPIO(HEARTBEAT_LED_GPIO_Port, HEARTBEAT_LED_Pin, 1);
+        break;
 
-        case 1:
-            PHAL_writeGPIO(CONN_LED_GPIO_Port, CONN_LED_Pin, 1);
-            break;
+    case 1:
+        PHAL_writeGPIO(CONN_LED_GPIO_Port, CONN_LED_Pin, 1);
+        break;
 
-        case 2:
-            PHAL_writeGPIO(ERROR_LED_GPIO_Port, ERROR_LED_Pin, 1);
-            break;
+    case 2:
+        PHAL_writeGPIO(ERROR_LED_GPIO_Port, ERROR_LED_Pin, 1);
+        break;
     }
 }
 
@@ -233,13 +227,13 @@ void preflightAnimation(void)
 void heartbeatTask()
 {
     if ((sched.os_ticks - last_can_rx_time_ms) >= 500)
-         PHAL_writeGPIO(CONN_LED_GPIO_Port, CONN_LED_Pin, 0);
-    else PHAL_writeGPIO(CONN_LED_GPIO_Port, CONN_LED_Pin, 1);
+        PHAL_writeGPIO(CONN_LED_GPIO_Port, CONN_LED_Pin, 0);
+    else
+        PHAL_writeGPIO(CONN_LED_GPIO_Port, CONN_LED_Pin, 1);
     PHAL_toggleGPIO(HEARTBEAT_LED_GPIO_Port, HEARTBEAT_LED_Pin);
 
     SEND_PRECHARGE_HB(q_tx_can, !PHAL_readGPIO(IMD_STATUS_GPIO_Port, IMD_STATUS_Pin), orionErrors());
 }
-
 
 void monitorStatus()
 {
@@ -252,24 +246,20 @@ void monitorStatus()
     PHAL_writeGPIO(ERROR_LED_GPIO_Port, ERROR_LED_Pin, bms_err | imd_err);
 }
 
-
-
 void sendIMUData()
 {
     imu_periodic(&imu_h);
 }
 
-
 // *** Compulsory CAN Tx/Rx callbacks ***
 void canTxUpdate()
 {
     CanMsgTypeDef_t tx_msg;
-    if (qReceive(&q_tx_can, &tx_msg) == SUCCESS_G)    // Check queue for items and take if there is one
+    if (qReceive(&q_tx_can, &tx_msg) == SUCCESS_G) // Check queue for items and take if there is one
     {
         PHAL_txCANMessage(&tx_msg);
     }
 }
-
 
 void CAN1_RX0_IRQHandler()
 {
@@ -286,31 +276,30 @@ void CAN1_RX0_IRQHandler()
         // Get either StdId or ExtId
         if (CAN_RI0R_IDE & CAN1->sFIFOMailBox[0].RIR)
         {
-          rx.ExtId = ((CAN_RI0R_EXID | CAN_RI0R_STID) & CAN1->sFIFOMailBox[0].RIR) >> CAN_RI0R_EXID_Pos;
+            rx.ExtId = ((CAN_RI0R_EXID | CAN_RI0R_STID) & CAN1->sFIFOMailBox[0].RIR) >> CAN_RI0R_EXID_Pos;
         }
         else
         {
-          rx.StdId = (CAN_RI0R_STID & CAN1->sFIFOMailBox[0].RIR) >> CAN_TI0R_STID_Pos;
+            rx.StdId = (CAN_RI0R_STID & CAN1->sFIFOMailBox[0].RIR) >> CAN_TI0R_STID_Pos;
         }
 
         rx.Bus = CAN1;
         rx.DLC = (CAN_RDT0R_DLC & CAN1->sFIFOMailBox[0].RDTR) >> CAN_RDT0R_DLC_Pos;
 
-        rx.Data[0] = (uint8_t) (CAN1->sFIFOMailBox[0].RDLR >> 0) & 0xFF;
-        rx.Data[1] = (uint8_t) (CAN1->sFIFOMailBox[0].RDLR >> 8) & 0xFF;
-        rx.Data[2] = (uint8_t) (CAN1->sFIFOMailBox[0].RDLR >> 16) & 0xFF;
-        rx.Data[3] = (uint8_t) (CAN1->sFIFOMailBox[0].RDLR >> 24) & 0xFF;
-        rx.Data[4] = (uint8_t) (CAN1->sFIFOMailBox[0].RDHR >> 0) & 0xFF;
-        rx.Data[5] = (uint8_t) (CAN1->sFIFOMailBox[0].RDHR >> 8) & 0xFF;
-        rx.Data[6] = (uint8_t) (CAN1->sFIFOMailBox[0].RDHR >> 16) & 0xFF;
-        rx.Data[7] = (uint8_t) (CAN1->sFIFOMailBox[0].RDHR >> 24) & 0xFF;
+        rx.Data[0] = (uint8_t)(CAN1->sFIFOMailBox[0].RDLR >> 0) & 0xFF;
+        rx.Data[1] = (uint8_t)(CAN1->sFIFOMailBox[0].RDLR >> 8) & 0xFF;
+        rx.Data[2] = (uint8_t)(CAN1->sFIFOMailBox[0].RDLR >> 16) & 0xFF;
+        rx.Data[3] = (uint8_t)(CAN1->sFIFOMailBox[0].RDLR >> 24) & 0xFF;
+        rx.Data[4] = (uint8_t)(CAN1->sFIFOMailBox[0].RDHR >> 0) & 0xFF;
+        rx.Data[5] = (uint8_t)(CAN1->sFIFOMailBox[0].RDHR >> 8) & 0xFF;
+        rx.Data[6] = (uint8_t)(CAN1->sFIFOMailBox[0].RDHR >> 16) & 0xFF;
+        rx.Data[7] = (uint8_t)(CAN1->sFIFOMailBox[0].RDHR >> 24) & 0xFF;
 
-        CAN1->RF0R     |= (CAN_RF0R_RFOM0);
+        CAN1->RF0R |= (CAN_RF0R_RFOM0);
         canProcessRxIRQs(&rx);
         qSendToBack(&q_rx_can, &rx); // Add to queue (qSendToBack is interrupt safe)
     }
 }
-
 
 void CAN2_RX0_IRQHandler()
 {
@@ -328,25 +317,25 @@ void CAN2_RX0_IRQHandler()
         // Get either StdId or ExtId
         if (CAN_RI0R_IDE & CAN2->sFIFOMailBox[0].RIR)
         {
-          rx.ExtId = ((CAN_RI0R_EXID | CAN_RI0R_STID) & CAN2->sFIFOMailBox[0].RIR) >> CAN_RI0R_EXID_Pos;
+            rx.ExtId = ((CAN_RI0R_EXID | CAN_RI0R_STID) & CAN2->sFIFOMailBox[0].RIR) >> CAN_RI0R_EXID_Pos;
         }
         else
         {
-          rx.StdId = (CAN_RI0R_STID & CAN2->sFIFOMailBox[0].RIR) >> CAN_TI0R_STID_Pos;
+            rx.StdId = (CAN_RI0R_STID & CAN2->sFIFOMailBox[0].RIR) >> CAN_TI0R_STID_Pos;
         }
 
         rx.DLC = (CAN_RDT0R_DLC & CAN2->sFIFOMailBox[0].RDTR) >> CAN_RDT0R_DLC_Pos;
 
-        rx.Data[0] = (uint8_t) (CAN2->sFIFOMailBox[0].RDLR >> 0) & 0xFF;
-        rx.Data[1] = (uint8_t) (CAN2->sFIFOMailBox[0].RDLR >> 8) & 0xFF;
-        rx.Data[2] = (uint8_t) (CAN2->sFIFOMailBox[0].RDLR >> 16) & 0xFF;
-        rx.Data[3] = (uint8_t) (CAN2->sFIFOMailBox[0].RDLR >> 24) & 0xFF;
-        rx.Data[4] = (uint8_t) (CAN2->sFIFOMailBox[0].RDHR >> 0) & 0xFF;
-        rx.Data[5] = (uint8_t) (CAN2->sFIFOMailBox[0].RDHR >> 8) & 0xFF;
-        rx.Data[6] = (uint8_t) (CAN2->sFIFOMailBox[0].RDHR >> 16) & 0xFF;
-        rx.Data[7] = (uint8_t) (CAN2->sFIFOMailBox[0].RDHR >> 24) & 0xFF;
+        rx.Data[0] = (uint8_t)(CAN2->sFIFOMailBox[0].RDLR >> 0) & 0xFF;
+        rx.Data[1] = (uint8_t)(CAN2->sFIFOMailBox[0].RDLR >> 8) & 0xFF;
+        rx.Data[2] = (uint8_t)(CAN2->sFIFOMailBox[0].RDLR >> 16) & 0xFF;
+        rx.Data[3] = (uint8_t)(CAN2->sFIFOMailBox[0].RDLR >> 24) & 0xFF;
+        rx.Data[4] = (uint8_t)(CAN2->sFIFOMailBox[0].RDHR >> 0) & 0xFF;
+        rx.Data[5] = (uint8_t)(CAN2->sFIFOMailBox[0].RDHR >> 8) & 0xFF;
+        rx.Data[6] = (uint8_t)(CAN2->sFIFOMailBox[0].RDHR >> 16) & 0xFF;
+        rx.Data[7] = (uint8_t)(CAN2->sFIFOMailBox[0].RDHR >> 24) & 0xFF;
 
-        CAN2->RF0R     |= (CAN_RF0R_RFOM0);
+        CAN2->RF0R |= (CAN_RF0R_RFOM0);
         canProcessRxIRQs(&rx);
         qSendToBack(&q_rx_can, &rx); // Add to queue (qSendToBack is interrupt safe)
     }
